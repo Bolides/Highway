e7edaedec71f1daed577d2bf964840f6eb16add0
Stijn Willems <stijn.willems@dooz.be>
2018-11-29 17:58:32 +0100

    removing build scripts in favor of carthage

--- scripts/loc.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd Sources
-locs=$(( find ./ -name '*.swift' -print0 | xargs -0 cat ) | wc -l)
-DATE=`date '+%Y-%m-%d %H:%M:%S'`
-echo "${DATE}                ${locs}" >> ./../scripts/loc_over_time.text
-
-cat ./../scripts/loc_over_time.text
--- scripts/tag.sh
+++
@@ -1,5 +0,0 @@
-git add .
-git commit -m "update"
-git-autotag
-git push origin master
-git push --tags
