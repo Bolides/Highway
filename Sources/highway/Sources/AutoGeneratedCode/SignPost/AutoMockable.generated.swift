a763b4eb2f9e0084150571430edadb838402ca47
Stijn Willems <stijn.willems@dooz.be>
2019-03-05 17:40:24 +0100

    Add SignPost and SignPostMock

--- Sources/highway/Sources/AutoGeneratedCode/SignPost/AutoMockable.generated.swift
+++ Sources/highway/Sources/AutoGeneratedCode/SignPost/AutoMockable.generated.swift
@@ -2,9 +2,6 @@ import Foundation
 import os
 import SignPost
 import SourceryAutoProtocols
-import ZFile
-import ZFileMock
-
 
 // Generated using Sourcery 0.15.0 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
