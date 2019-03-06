58ee89ef843075a22b259b728252947253896869
Stijn Willems <stijn.willems@dooz.be>
2019-03-06 21:33:40 +0100

    Added HWSetup to generate code with sourcery and do other setup stuff that used to be done by AutomateHighway

--- HighwaySetup/Package.swift
+++ HighwaySetup/Package.swift
@@ -11,6 +11,7 @@ package.dependencies = [
     .package(url: "https://github.com/doozMen/Highway.git", from: "2.2.0"),
     .package(url: "https://www.github.com/doozMen/template-sourcery", from: "1.2.0"),
     .package(url: "https://www.github.com/doozMen/ZFile", from: "2.0.1"),
+    .package(url: "https://www.github.com/doozMen/Sourcery", from: "0.16.2")
 ]
 package.targets = [
     .target(name: "HighwaySetup", dependencies: ["Arguments", "Errors", "SwiftFormatWorker", "Terminal", "ZFile", "SourceryWorker", "SourceryAutoProtocols", "ZFile", "SignPost"], path: "Sources")
