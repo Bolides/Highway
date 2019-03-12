13dabcbf244e1ed591a2fa9e97b888a52f5217b8
doozMen <stijn@dooz.be>
2019-03-12 19:10:04 +0100

    update sourcery templates

--- Package.swift
+++ Package.swift
@@ -76,7 +76,7 @@ public let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://www.github.com/doozMen/ZFile", "2.0.2"..<"3.1.0"),
         .package(url: "https://www.github.com/doozMen/SignPost", from: "1.0.0"),
-        .package(url: "https://www.github.com/doozMen/template-sourcery", "1.3.1"..<"2.1.0"),
+        .package(url: "https://www.github.com/doozMen/template-sourcery", "1.3.2"..<"2.1.0"),
         .package(url: "https://www.github.com/antitypical/Result", "4.1.0"..<"5.1.0"),
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.39.4"),
         .package(url: "https://www.github.com/Quick/Quick", "1.3.4"..<"2.1.0"),
