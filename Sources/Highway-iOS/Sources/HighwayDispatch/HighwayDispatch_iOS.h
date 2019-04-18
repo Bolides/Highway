2e3fa4e9dcfbddf058ae95c668cca16bf4c92e20
doozMen <stijn@dooz.be>
2019-04-18 12:10:47 +0200

    Add iOS support for products usefull in iOS Errors and HighwayDispatch

---
+++ Sources/Highway-iOS/Sources/HighwayDispatch/HighwayDispatch_iOS.h
@@ -0,0 +1,19 @@
+//
+//  HighwayDispatch_iOS.h
+//  HighwayDispatch-iOS
+//
+//  Created by Stijn Willems on 18/04/2019.
+//  Copyright © 2019 Stijn Willems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HighwayDispatch_iOS.
+FOUNDATION_EXPORT double HighwayDispatch_iOSVersionNumber;
+
+//! Project version string for HighwayDispatch_iOS.
+FOUNDATION_EXPORT const unsigned char HighwayDispatch_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HighwayDispatch_iOS/PublicHeader.h>
+
+
